1bcbd34de3ee7cbfd334244ac235b4215b33c048
verytouch <>
2020-09-29 17:44:07 +0800

    MapUtils

---
+++ common/src/test/java/com/verytouch/vkit/common/util/MapUtilsTest.java
@@ -0,0 +1,25 @@
+package com.verytouch.vkit.common.util;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+public class MapUtilsTest {
+
+    @Test
+    public void testCreate() {
+        final Map<String, Object> map = MapUtils.singleEntryHashMap("哈", "很");
+        System.out.println(map);
+    }
+
+    @Test
+    public void tesBuilder() {
+        final Map<String, Object> map = MapUtils.Builder.hashMap()
+                .put("key", "124")
+                .put("page", 1)
+                .put("size", 20)
+                .build();
+        System.out.println(map);
+    }
+
+}
