b17988ed0ff47c348bb487789e0d3c8c739d6f5b
zhouwei <>
2021-05-25 17:59:27 +0800

    refactor OauthUtils

--- starters/vkit-spring-boot-autoconfigure/src/main/java/top/verytouch/vkit/rabc/util/Oauth2Utils.java
+++ starters/vkit-spring-boot-autoconfigure/src/main/java/top/verytouch/vkit/rabc/util/Oauth2Utils.java
@@ -1,15 +1,16 @@
 package top.verytouch.vkit.rabc.util;
 
-import top.verytouch.vkit.common.util.JsonUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import top.verytouch.vkit.common.util.JsonUtils;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -21,42 +22,48 @@ import java.util.stream.Collectors;
 public class Oauth2Utils {
 
     /**
-     * 获取用户名
+     * 获取认证信息
      */
-    public static String getUsername() {
-        return SecurityContextHolder.getContext().getAuthentication().getName();
+    public static Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
     }
 
     /**
-     * 获取认证信息
+     * 获取用户名
      */
-    public static OAuth2AuthenticationDetails getDetails() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return (OAuth2AuthenticationDetails) authentication.getDetails();
+    public static String getUsername() {
+        return getAuthentication().getName();
     }
 
     /**
      * 获取权限信息
      */
-    public static List<String> getAuthorities() {
-        return SecurityContextHolder.getContext().getAuthentication()
+    public static Set<String> getAuthorities() {
+        return getAuthentication()
                 .getAuthorities().stream()
-                .map(auth -> ((GrantedAuthority) auth).getAuthority())
-                .collect(Collectors.toList());
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet());
     }
 
     /**
      * 获取access_token
      */
     public static String getAccessToken() {
-        return getDetails().getTokenValue();
+        return ((OAuth2AuthenticationDetails) getAuthentication().getDetails()).getTokenValue();
     }
 
     /**
      * 获取jwt中自定义的信息
      */
-    public static Map<String, Object> getJwtUserInfo() {
-        String claims = JwtHelper.decode(getDetails().getTokenValue()).getClaims();
+    public static Map<String, Object> getJwtClaims() {
+        String claims = JwtHelper.decode(getAccessToken()).getClaims();
         return JsonUtils.fromJson(claims, HashMap.class);
+    }
+
+    /**
+     * 获取token过期时间戳
+     */
+    public static long getJwtExpireAt() {
+        return Long.parseLong(Objects.toString(getJwtClaims().get("exp"))) * 1000;
     }
 }
