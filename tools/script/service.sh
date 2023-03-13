9559c256a2d3337aa35ccbe76f0527191049f643
verytouch <verytouch@qq.com>
2023-03-13 11:43:22 +0800

    update service.sh

--- tools/script/service.sh
+++ tools/script/service.sh
@@ -1,24 +1,25 @@
 APP_NAME=$2
+TIPS="two params needed: param1=start|stop|restart|check, param2=app_name"
 
 if [ "" == "$APP_NAME" ]; then
-  echo "two params needed: param1=start|stop|restart|status, param2=app_name"
+  echo "$TIPS"
   exit 0
 fi
 
-check() {
-  pid=`pgrep -f $APP_NAME.jar`
+find_pid() {
+  pid=$(pgrep -f "$APP_NAME".jar)
 }
 
 start() {
-  check
-  if [ "" != "$pid" ]; then
+  find_pid
+  if [ -n "$pid" ]; then
     echo "$APP_NAME already running, pid=$pid"
   else
-    echo "starting $APP_NAME.jar"
-    nohup java -server -Xmx128m -Xloggc:$APP_NAME.gc -jar $APP_NAME.jar > $APP_NAME.log 2>&1 &
+    echo "starting $APP_NAME.jar..."
+    nohup java -server -Xmx128m -Xloggc:"$APP_NAME".gc -jar "$APP_NAME".jar > "$APP_NAME".log 2>&1 &
     sleep 3
-    check
-    if [ "" != "$pid" ]; then
+    find_pid
+    if [ -n "$pid" ]; then
       echo "$APP_NAME start success, pid=$!"
     else
       echo "$APP_NAME start failed in 3s"
@@ -27,29 +28,38 @@ start() {
 }
 
 stop() {
-  check
+  find_pid
   if [ -z "$pid" ]; then
       echo "$APP_NAME not running"
     else
-      kill -9 $pid
-      echo "$APP_NAME stoped, pid=$pid"
+      echo "stopping $APP_NAME.jar using kill..."
+      kill "$pid"
+      sleep 3
+      find_pid
+      if [ -z "$pid" ]; then
+        echo "$APP_NAME stop success"
+      else
+        echo "$APP_NAME stop failed in 3s"
+        echo "stopping $APP_NAME.jar using kill -9..."
+        kill -9 "$pid"
+        echo "$APP_NAME stop success"
+      fi
   fi
 }
 
 restart() {
   stop
-  sleep 3
   start
 }
 
 case "$1" in
-  "status")
-    check
+  "check")
+    find_pid
     if [ -z "$pid" ]; then
       echo "$APP_NAME not running"
     else
       echo "$APP_NAME is running, pid=$pid"
-   fi
+    fi
   ;;
   "start")
     start
@@ -61,7 +71,7 @@ case "$1" in
     restart
   ;;
   *)
-    echo "two params needed: param1=start|stop|restart|status, param2=app_name"
+    echo "$TIPS"
   ;;
 esac
 
