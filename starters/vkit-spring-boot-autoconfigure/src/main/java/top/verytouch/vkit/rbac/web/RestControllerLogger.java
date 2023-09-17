ae1294c33c551e58f09ec4e95c586dccf77cbfbc
verytouch <verytouch@qq.com>
2023-09-17 21:44:36 +0800

    fix rest controller logger

--- starters/vkit-spring-boot-autoconfigure/src/main/java/top/verytouch/vkit/rbac/web/RestControllerLogger.java
+++ starters/vkit-spring-boot-autoconfigure/src/main/java/top/verytouch/vkit/rbac/web/RestControllerLogger.java
@@ -1,11 +1,9 @@
 package top.verytouch.vkit.rbac.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.web.multipart.MultipartFile;
 import top.verytouch.vkit.common.util.JsonUtils;
 
@@ -24,32 +22,37 @@ import java.util.stream.Collectors;
 @Slf4j
 public class RestControllerLogger {
 
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.RequestMapping) || " +
+    @Around("@annotation(org.springframework.web.bind.annotation.RequestMapping) || " +
             "@annotation(org.springframework.web.bind.annotation.GetMapping) || " +
             "@annotation(org.springframework.web.bind.annotation.PostMapping) || " +
             "@annotation(org.springframework.web.bind.annotation.PutMapping) || " +
             "@annotation(org.springframework.web.bind.annotation.PatchMapping) || " +
             "@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
-    public void webLog() {
-    }
-
-    @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) {
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
         if (log.isInfoEnabled()) {
-            String args = Arrays.stream(joinPoint.getArgs())
+            String clazz = joinPoint.getTarget().getClass().getSimpleName();
+            String method = joinPoint.getSignature().getName();
+            String params = Arrays.stream(args)
                     .filter(obj -> !(obj instanceof MultipartFile))
                     .filter(obj -> !(obj instanceof HttpServletRequest))
                     .filter(obj -> !(obj instanceof HttpServletResponse))
                     .map(JsonUtils::toJson)
                     .collect(Collectors.joining(", "));
-            log.info("request ===> {}.{}: {}", joinPoint.getTarget().getClass().getCanonicalName(), joinPoint.getSignature().getName(), args);
+            log.info("request ===> {}.{}: {}", clazz, method, params);
         }
-    }
-
-    @AfterReturning(returning = "response", pointcut = "webLog()")
-    public void doAfterReturning(Object response) {
-        if (log.isInfoEnabled()) {
-            log.info("response ==> {}", JsonUtils.toJson(response));
+        Object response;
+        try {
+            response = joinPoint.proceed(args);
+            if (log.isInfoEnabled()) {
+                log.info("response ===> {}", response);
+            }
+        } catch (Throwable e) {
+            if (log.isInfoEnabled()) {
+                log.info("response ===> ", e);
+            }
+            throw e;
         }
+        return response;
     }
 }
